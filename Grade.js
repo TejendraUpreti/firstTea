d114bcac9efa2f2ed1a449843e1119f520fad22e
Tejendra_Upreti <tejendraupreti@outlook.com>
2024-04-11 12:28:44 +0530

    Correct file for the grade in JS

--- Grade.js
+++ Grade.js
@@ -1,17 +1,20 @@
-let scor = prompt(Enter the Mark numbers then finaliez the Grade!);
+let scor = prompt("Enter the Mark numbers then finaliez the Grade!");
 let grade;
 
-if(scor <= 90 && scor >= 100){
+if(scor >= 90 && scor <= 100){
     grade = "A";
-}else if(scor <=70 && scor >= 89){
+}
+else if(scor >=70 && scor <=89){
     grade = "B";
-}else if(scor <=60 && scor >=69){
+}
+else if(scor >=60 && scor <=69){
     grade = "C";
-}else if(scor <=50 && scor >=59){
-    grade = 'D';
-}else if(scor <=0 && scor >=49){
+}
+else if(scor >=50 && scor <=59){
+    grade = "D";
+}
+else if(scor >=0 && scor <=49){
     grade = "E";
 }
     console.log(grade);
-
 
