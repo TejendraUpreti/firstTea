e62bea4d4cd8bc04e001c142d8930f93edddba37
Tejendra_Upreti <tejendraupreti@outlook.com>
2024-04-16 18:39:54 +0530

    small game in javaScript

---
+++ game.js
@@ -0,0 +1,10 @@
+
+let gameNum = 30;
+
+let userNum = prompt("Guess a correct number for the entertainment: ");
+
+while(userNum != gameNum){
+    
+    userNum = prompt("Wrong Number! Guess again😁");
+}
+console.log("Congratulations you finally guessed the right number ✔ ")
