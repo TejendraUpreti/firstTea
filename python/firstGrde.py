200c3e768c80653017e4f851e72c17628045267a
Tejendra_Upreti <tejendraupreti@outlook.com>
2024-04-25 19:20:51 +0530

    this is html/css code for the some unique html

---
+++ python/firstGrde.py
@@ -0,0 +1,12 @@
+# find the grade in the python
+
+mark = int(input("Find Grade: "))
+
+if(mark >= 90):
+    print("A")
+elif(mark >= 80 and mark < 90):
+    print("B")
+elif(mark >= 70 and mark < 80):
+    print("C")
+else:
+    print("Fall")            
