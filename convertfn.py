045fcd23db492aa9e51f8f9d79d9c3304a6562b9
Tejendra_Upreti <tejendraupreti777@yahoo.com>
2024-05-05 16:54:49 +0530

    Converting USD to rupees

---
+++ convertfn.py
@@ -0,0 +1,8 @@
+value = int(input("Enter the USD to Convert the Rupees: "))
+
+def myfunction(usd_value):
+    
+    inr_value = usd_value * 83
+    print(usd_value, "USD =", inr_value,"INR")
+
+myfunction(value)    
